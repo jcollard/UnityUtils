90ce53cc4773e4f8d13423dc524b61a73c3912f2
Joseph Collard <jcollard@chadwickschool.org>
2021-11-28 10:08:32 -0800

    feat: Initial commit of UnityUtils.

---
+++ CaptainCoder/Utils/UnityEditorUtils.cs
@@ -0,0 +1,62 @@
+#if UNITY_EDITOR
+namespace CaptainCoder.Utils
+{
+    using UnityEngine;
+    using UnityEditor;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System;
+
+    public class UnityEditorUtils
+    {
+        public static readonly UnityEditorUtils Instance = new UnityEditorUtils();
+
+        private UnityEditorUtils() {}
+
+        private static readonly Dictionary<Color, GUIStyle> ColorLabels = new Dictionary<Color, GUIStyle>();
+        private static GUIStyle _MonoSpacedTextArea;
+
+        /// <summary>
+        /// Builds a GUIStyle for a Label setting the color of the label to the specified color.
+        /// </summary>
+        /// <param name="color">The color of the text in the GUIStyle.</param>
+        /// <returns></returns>
+        public static GUIStyle GetColorLabel(Color color)
+        {
+            if (!ColorLabels.TryGetValue(color, out GUIStyle style))
+            {
+                style = new GUIStyle(EditorStyles.label);
+                style.normal.textColor = color;
+            }
+
+            return style;
+        }
+
+        /// <summary>
+        /// A GUIStyle for a MonoSpacedTextArea. This assumes there is a font file
+        /// named "Fonts/MonoSpaced" within your Resources folder.
+        /// </summary>
+        public static GUIStyle MonoSpacedTextArea
+        {
+            get
+            {
+                if (_MonoSpacedTextArea == null)
+                {
+                    _MonoSpacedTextArea = new GUIStyle(EditorStyles.textArea);
+                    Font f = Resources.Load<Font>("Fonts/MonoSpaced");
+                    if (f == null)
+                    {
+                        Debug.LogWarning("Attempted to load font from 'Resources/Fonts/MonoSpaced' but no file was found. Defaulting to system font.");
+                    }
+                    else
+                    {
+                        _MonoSpacedTextArea.font = f;
+                        _MonoSpacedTextArea.fontSize = 18;
+                    }
+                }
+                return _MonoSpacedTextArea;
+            }
+        }
+    }
+}
+#endif
---
+++ CaptainCoder/Utils/UnityEngineUtils.cs
@@ -0,0 +1,29 @@
+namespace CaptainCoder.Utils
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System;
+
+    public class UnityEngineUtils
+    {
+        public static readonly UnityEngineUtils Instance = new UnityEngineUtils();
+
+        private UnityEngineUtils() {}
+
+        /// <summary>
+        /// Given a Transform, calls DestroyImmediate on each of the children
+        /// contained in that transform.
+        /// </summary>
+        /// <param name="t">The container to empty.</param>
+        public void DestroyImmediateChildren(Transform t)
+        {
+            List<Transform> children = t.Cast<Transform>().ToList();
+            foreach (Transform child in children)
+            {
+                UnityEngine.Object.DestroyImmediate(child.gameObject);
+            }
+        }
+
+    }
+}
